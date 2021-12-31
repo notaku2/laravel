911aa8c97e403bb4c3a6d327cacf51bdc58fa5c7
notaku2 <nontaku222@gmail.com>
2021-12-31 17:21:09 +0900

    QuickAdminPanel_Finish

--- src/laradock/app/swagger.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * @OA\Info(
- *     version="1.0.0",
- *     title="Swagger",
- *     description="OpenAPI",
- * )
- */
--- src/laradock/routes/api.php
+++ src/laradock/routes/api.php
@@ -13,7 +13,16 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+/*
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+*/
+Route::group([
+    'prefix' => 'v1', 
+    'as' => 'api.', 
+    'namespace' => 'Api\V1\Admin', 
+    'middleware' => ['auth:api']
+  ], function () {
+      Route::apiResource('projects', 'ProjectsApiController');
+  });
