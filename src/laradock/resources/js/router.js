bc4e18e73958373916cb299ff65b0cf644b609b0
notaku2 <nontaku222@gmail.com>
2022-02-19 14:12:20 +0900

    all_add_files

---
+++ src/laradock/resources/js/router.js
@@ -0,0 +1,25 @@
+//import Vue from 'vue'
+//import VueRouter from 'vue-router'
+/*
+import * as Vue from 'vue';
+import * as VueRouter from 'vue-router';
+
+//Vue.use(VueRouter);
+import { createRouter, createWebHistory } from 'vue-router'
+
+// コンポーネントをインポート
+import HeaderComponent from "./components/HeaderComponent";
+import TaskListComponent from "./components/TaskListComponent";
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/tasks',
+            name: 'task.list',
+            component: TaskListComponent
+        },
+    ]
+});
+export default router;
+*/
