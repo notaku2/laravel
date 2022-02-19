c85cab95f1cf62f00667359deb5468b693753121
notaku2 <nontaku222@gmail.com>
2022-02-19 13:52:56 +0900

    Main_front_view_file

--- src/laradock/resources/views/welcome.blade.php
+++ src/laradock/resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link href="{{ mix('css/app.css') }}" rel="stylesheet" type="text/css">
 
         <title>Laravel_Vue</title>
     </head>
