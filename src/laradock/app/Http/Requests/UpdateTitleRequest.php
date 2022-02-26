e08ff40ea2be8523b4038008f4b88d979c785d33
notaku2 <nontaku222@gmail.com>
2022-02-26 14:30:54 +0900

    Changed_UpdateTitleRequest

--- src/laradock/app/Http/Requests/UpdateTitleRequest.php
+++ src/laradock/app/Http/Requests/UpdateTitleRequest.php
@@ -8,11 +8,13 @@ use Symfony\Component\HttpFoundation\Response;
 
 class UpdateTitleRequest extends FormRequest
 {
+    /*
     public function authorize()
     {
         abort_if(Gate::denies('title_edit'), Response::HTTP_FORBIDDEN, '403 Forbidden');
         return true;
     }
+    */
 
     public function rules()
     {
