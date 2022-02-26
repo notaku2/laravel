b2362be0218123fa0dac5f69500280391f18d71b
notaku2 <nontaku222@gmail.com>
2022-02-26 14:59:38 +0900

    Changed_StoreQuestionRequest

--- src/laradock/app/Http/Requests/StoreQuestionRequest.php
+++ src/laradock/app/Http/Requests/StoreQuestionRequest.php
@@ -8,11 +8,13 @@ use Symfony\Component\HttpFoundation\Response;
 
 class StoreQuestionRequest extends FormRequest
 {
+    /*
     public function authorize()
     {
         abort_if(Gate::denies('question_create'), Response::HTTP_FORBIDDEN, '403 Forbidden');
         return true;
     }
+    */
 
     public function rules()
     {
