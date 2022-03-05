2792d776c24d3d13f3eeed039e4d172287e263a3
notaku2 <nontaku222@gmail.com>
2022-03-05 15:54:48 +0900

    Changed_Swgg_Requests_Store_Add_ForeignId

--- src/laradock/app/Swgg/Requests/StoreQuestionRequest.php
+++ src/laradock/app/Swgg/Requests/StoreQuestionRequest.php
@@ -5,7 +5,7 @@
  *      title="Store Question request",
  *      description="Store Question request body data",
  *      type="object",
- *      required={"character","line"}
+ *      required={"character","line","user_id","title_id","chapter_id"}
  * )
  */
 
@@ -32,6 +32,30 @@ class StoreQuestionRequest
      * @var string
      */
     public $line;
+
+    /**
+     * @OA\Property(
+     *      title="user_id",
+     *      description="Title's id of the new chapter",
+     *      format="int64",
+     *      example=1
+     * )
+     *
+     * @var integer
+     */
+    public $user_id;
+
+    /**
+     * @OA\Property(
+     *      title="title_id",
+     *      description="Title's id of the new chapter",
+     *      format="int64",
+     *      example=1
+     * )
+     *
+     * @var integer
+     */
+    public $title_id;
 
     /**
      * @OA\Property(
